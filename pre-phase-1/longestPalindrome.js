c9d37b60af60ba9df93d5f71e1fd5a2dbabcd163
rahmandihamzah <rahmandi.dev@gmail.com>
2020-01-03 09:54:40 +0700

    phase 1 warming up

--- pre-phase-1/longestPalindrome.js
+++ pre-phase-1/longestPalindrome.js
@@ -18,7 +18,49 @@
  */
 
 function longestPalindrome(str) {
-  // kode disini
+  let tempStr = '';
+  let longestPalindromeResult = '';
+
+  for (let i = 0; i < str.length; i++) {
+    let newStr = '';
+
+    for (let j = i; j < str.length; j++) { // new string start from i
+      newStr += str[j];
+    }
+    // console.log(newStr);
+
+    let newStr1 = '';
+    let k = 0;
+    let status = true;
+    while (status) {
+      for (let l = 0; l < newStr.length - k; l++) {
+        newStr1 += newStr[l];
+      }
+      // console.log(newStr1);
+
+      for (let m = newStr1.length - 1; m >= 0; m--) {
+        tempStr += newStr1[m];
+      }
+      // console.log('ini temp ' + tempStr + '\n============');
+
+      if (newStr1 === tempStr) {
+        // console.log(newStr);
+        if (newStr1.length > longestPalindromeResult.length) {
+          longestPalindromeResult = newStr1;
+        }
+      }
+
+      tempStr = '';
+
+      if (k === newStr.length - 1) {
+        status = false;
+      } else {
+        k++;
+        newStr1 = '';
+      }
+    }
+  }
+  console.log(longestPalindromeResult);
 }
 
 // Test Case
